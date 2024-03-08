018694d0c04229eee8513b1bb57e49228321ed56
heyvipul <vipulgirhestar@gmail.com>
2024-03-08 20:55:27 +0530

    i-dependencies

---
+++ src/constants/config.js
@@ -0,0 +1,7 @@
+
+export const chartConfig = {
+    "1D" : {days : 1 ,week : 0, months : 0, year :0, resolution : "1"},
+    "1W" : {days : 0 ,week : 1, months : 0, year :0, resolution : "15"},
+    "1M" : {days : 0 ,week : 0, months : 1, year :0, resolution : "60"},
+    "1Y" : {days : 0 ,week : 0, months : 0, year :1, resolution : "D"},
+}
