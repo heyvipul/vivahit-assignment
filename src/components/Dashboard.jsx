85fc5346e036de797961f6a255b87532577f3bac
heyvipul <vipulgirhestar@gmail.com>
2024-03-09 14:16:32 +0530

    chart-comp

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { mockCompanyDetails } from '../constants/mock.js'
 import Card from './Card.jsx'
+import Chart from './Chart.jsx'
 import Details from './Details.jsx'
 import Header from './Header.jsx'
 import Overview from './Overview.jsx'
@@ -7,14 +8,15 @@ import Overview from './Overview.jsx'
 
 const Dashboard = () => {
   return (
-    <div className='h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8     md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand'>
+    <div className='h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8     md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand bg-neutral-100'>
 
         <div className='col-span-1 md:col-span-2 xl:col-span-3 row-span-1 flex justify-start items-center'>
             {/* <Card>Header</Card> */}
             <Header name={mockCompanyDetails.name}/>
         </div>
         <div className='md:col-span-2 row-span-4'>
-            <Card>Chart</Card>
+            {/* <Card>Chart</Card> */}
+            <Chart/>
         </div>
         <div>
             {/* <Card>Overview</Card> */}
---
+++ src/helpers/date.helper.js
@@ -0,0 +1,18 @@
+//date.getTime return no seconds
+
+//function to convert from no seconds to sec
+export const convertDatetoUnixTimestamp = (date) => {
+    return Math.floor(date.getTime() / 1000);
+};
+
+export const converUnixTimestamptoDate = (unixTimestamp) =>{
+    const milisec = unixTimestamp * 1000;
+    return new Date(milisec).toLocaleDateString();
+}
+
+export const createDate = (date,days,weeks,months,years)=>{
+    let newDate = new Date(date);
+    newDate.setDate(newDate.getDate() + days + 7*weeks);
+    newDate.setMonth(newDate.getMonth() + months);
+    newDate.setFullYear(newDate.getFullYear() + years);
+}
